d4ba0f61e88da357dc5695b5dd907bcf788a94a3
51928-lin <13148880908@163.com>
2024-12-20 14:54:01 +0800

    test

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
+  // 测试
   document.getElementById('root') as HTMLElement
 );
 root.render(
